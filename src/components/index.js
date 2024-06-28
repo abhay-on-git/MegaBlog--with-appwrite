419319cffdeadf4b163af64c621771fc1b8180de
unknown <abhayagnihotri1585@gmail.com>
2024-06-28 20:00:23 +0530

    some more components

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,15 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Logo from "./Logo"
 import Container from "./container/Container";
+import Button from "./Button"
+import Input from "./Input";
+import Login from "./Login";
 export  {
     Header,
     Footer,
     Logo,
-    Container
+    Container,
+    Button,
+    Input,
+    Login,
 }
