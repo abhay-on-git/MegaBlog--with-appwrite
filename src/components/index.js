1e92bdea49cae99d7b0c7d33821a92c96550d5f3
unknown <abhayagnihotri1585@gmail.com>
2024-07-05 19:23:34 +0530

    added-components-build-skeleton

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,30 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import Logo from "./Logo"
+import Logo from "./Logo";
 import Container from "./container/Container";
-import Button from "./Button"
+import Button from "./Button";
 import Input from "./Input";
 import Login from "./Login";
-export  {
-    Header,
-    Footer,
-    Logo,
-    Container,
-    Button,
-    Input,
-    Login,
-}
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import PostForm from "./post--form/postForm";
+import LogoutBtn from "./LogoutBtn";
+import AuthLayout from "./AuthLayout"
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  Button,
+  Input,
+  Login,
+  RTE,
+  Signup,
+  Select,
+  PostCard,
+  PostForm,
+  LogoutBtn,
+  AuthLayout
+};
